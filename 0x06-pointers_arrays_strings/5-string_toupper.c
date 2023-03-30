95166c197d8554f14e677282b0fe2eb1561fc1dd
playboyytom <thomasonyango420@gmail.com>
2023-03-30 12:52:33 -0700

    toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - change lower case to upper case
+ * @str: string to be converted
+ * Return: strings
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 'a' + 'A';
+	}
+	return (str);
+}
+
