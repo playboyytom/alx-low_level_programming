e41a576633bba5013d4b2a7f26dace12aed132c9
playboyytom <thomasonyango420@gmail.com>
2023-04-02 12:39:07 -0700

    eee

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,6 +18,16 @@ void print_to_98(int n)
 		{
 			printf("%d", i);
 			if (i != 98)
+				printf(", ");
+		}
+		printf("\n");
+	}
+	else if (n < 0)
+	{
+		for (i = n ; i <= 98 ; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
 				printf(",");
 		}
 		printf("\n");
@@ -28,7 +38,7 @@ void print_to_98(int n)
 		{
 			printf("%d", i);
 			if (i != 98)
-				printf(",");
+				printf(", ");
 		}
 		printf("\n");
 	}
