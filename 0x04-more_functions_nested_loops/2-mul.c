8c744d884d285df5fad5342400a1144c87b0fc20
playboyytom <thomasonyango420@gmail.com>
2023-04-02 21:15:12 -0700

    collaboration

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - ultipliers two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
